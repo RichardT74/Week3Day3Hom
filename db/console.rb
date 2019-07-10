f07c97e205f5528badde4ae1903e2601862c4653
matmic <madebymat@hotmail.com>
2019-07-10 16:07:25 +0100

    Save functions created in the classes

--- db/console.rb
+++ db/console.rb
@@ -3,12 +3,26 @@ require_relative('../models/album.rb')
 require_relative('../models/artist.rb')
 
 Artist.delete_all()
-# Album.delete_all()
+Album.delete_all()
 
 artist1 = Artist.new({'name' => 'Pink Floyd'})
 artist2 = Artist.new({'name' => 'The Beatles'})
 artist1.save()
 artist2.save()
+
+album1 = Album.new({
+  'title' => 'Animals',
+  'genre' => 'Prog rock',
+  'artist_id' => artist1.id
+  })
+
+  album2 = Album.new({
+    'title' => 'Let it Be',
+    'genre' => 'Legendary',
+    'artist_id' => artist2.id
+    })
+  album1.save()
+  album2.save()
 
 binding.pry
 nil
--- models/album.rb
+++ models/album.rb
@@ -13,6 +13,17 @@ class Album
     @artist_id = options['artist_id'].to_i
   end
 
+  def save()
+    sql = "INSERT INTO albums (title, genre, artist_id)
+    VALUES ($1, $2, $3)
+    RETURNING id"
+    values = [@title, @genre, @artist_id]
+    album_hash = returned_array[0]
+    id_string = album_hash['id']
+    @id = id_string.to_i
+  end
+
+
 
   def self.delete_all()
     sql = "DELETE FROM albums"
