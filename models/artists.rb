be660f4ed19dbace6407dab096224a7815dcf51a
matmic <madebymat@hotmail.com>
2019-07-10 15:07:47 +0100

    artist class created

---
+++ models/artists.rb
@@ -0,0 +1,14 @@
+require_relative('../db/sql_runner.rb')
+
+class Artists
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+  end
+
+
+
+
+
+end
